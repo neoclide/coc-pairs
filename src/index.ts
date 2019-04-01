d30414d6e97436ba5f72333745d0593c8e58516d
chemzqm <chemzqm@gmail.com>
2019-04-01 15:31:37 +0800

    disable backspace keymap for vim

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,7 @@ export async function activate(context: ExtensionContext): Promise<void> {
   if (enableBackspace) {
     let map = await workspace.nvim.call('maparg', ['<bs>', 'i']) as string
     if (map && !map.startsWith('coc#_insert_key')) enableBackspace = false
+    if (workspace.isVim) enableBackspace = false
   }
 
   if (characters.length == 0) return
